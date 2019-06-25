d171b0a25319bada96aafea78a2164f481909044
Takako <46050715+snickersty@users.noreply.github.com>
2019-06-25 14:21:02 -0400

    Update Untitled.py

--- Untitled.py
+++ Untitled.py
@@ -1,53 +1,10 @@
-"""June 25, 2019
-number = int(input("give me a number: "))
-
-if number > 0:
-    if number % 4 is 0:
-        print(number, "is an even number & can be divided by 4")
-    elif number % 2 is 0:
-        print(number, "is an even number")
-    else:
-        print(number, "is an odd number")
 """
-"""#3
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-a_2 = []
-
-for i in a:
-    if i<5:
-        a_2.append(i)
-
-print(a_2)
+Ask the user for a string and print out whether this string is a palindrome or not. 
+(A palindrome is a string that reads the same forwards and backwards.)
 """
 
-"""#4
-number = int(input("give me a number: "))
-divisors = []
-
-for i in range(2,number):
-    if number % i is 0:
-        divisors.append(i)
-
-print("divisors for your number", number, ": ", divisors)
-"""
-
-"""#5
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-
-c = []
-for i in a:
-    for e in b:
-        if i == e:
-            c.append(i)
-
-c = list(set(c))
-print(c)
-"""
-
-"""#6
 word = input("type in a string: ")
-###def reverse(word):
+def reverse(word):
     x = ""
     for i in range(len(word)):
         x += word[len(word)-1-i]
@@ -55,27 +12,22 @@ word = input("type in a string: ")
 
 x = reverse(word)
 print(x)
-###
+"""
+x COMES BACK WITH ONLY THE LAST LETTER AND DOESN'T LOOP
+"""
 
 word = input("type in a string: ")
-x = ""
-for i in range(len(word)):
-    x += word[len(word)-1-i]
-print(x)
+
 
 if x == word:
     print("this is a palindrome")
 else:
     print("this is NOT a palindrome")
 
-wrd = input("type in a string: ")
-wrd = str(wrd)
-rvs = wrd[::-1]
-print(rvs)
-if wrd == rvs:
-    print("this is a palindrome")
-else:
-    print("this is NOT a palindrome")
+    
+""" IF I DON'T DO DEF, IT WORKS FINE, LIKE BELOW:
+x = ""
+for i in range(len(word)):
+    x += word[len(word)-1-i]
+print(x)
 """
-
-#7
